75f6f03d660792a262f040b296376ff436d5826c
kion1491 <43998132+kion1491@users.noreply.github.com>
2020-07-06 21:26:51 +0900

    사용자생성함수 도입
    인자값으로 이미지 슬라이드의 옵션을 받아서 생성하는 방식 구현해봄.

--- img_slide_03/main.js
+++ img_slide_03/main.js
@@ -7,41 +7,50 @@ imgSrcList = [
   "http://placehold.it/1000x500/3e3e3e/ffffff?text=No.5"
 ];
 
-// DOM 초기화
-
-const slideBox = document.getElementById("slideBox");
-const slideReset = `
+// 사용자 정의 생성자 함수 방식
+function imgSlide(arrowColor) {
+  this.arrowColor = arrowColor;
+  const slideBox = document.getElementById("slideBox");
+  const slideReset = `
                     <div class="prev_arrow" id="prevArrow"></div>
                     <img src="`+ imgSrcList[0] + `" />
                     <div class="next_arrow" id="nextArrow"></div>
                    `;
-slideBox.innerHTML = slideReset;
+  slideBox.innerHTML = slideReset;
 
-const arrowColor = "#000";
-document.getElementById("prevArrow").style.borderRight = "50px solid " + arrowColor;
-document.getElementById("nextArrow").style.borderLeft = "50px solid " + arrowColor;
+  // const arrowColor = "#000";
+  document.getElementById("prevArrow").style.borderRight = "50px solid " + arrowColor;
+  document.getElementById("nextArrow").style.borderLeft = "50px solid " + arrowColor;
 
-slideImg = slideBox.firstChild.nextElementSibling.nextElementSibling;
-// console.log(slideImg);
-let i = 0;
+  slideImg = slideBox.firstChild.nextElementSibling.nextElementSibling;
+  // console.log(slideImg);
+  let i = 0;
 
-function slideNext() {
-  if (i == imgSrcList.length - 1) { i = -1 }
-  i = i + 1;
-  slideImg.src = imgSrcList[i];
-}
+  function slideNext() {
+    if (i == imgSrcList.length - 1) { i = -1 }
+    i = i + 1;
+    slideImg.src = imgSrcList[i];
+  }
 
-function slidePrev() {
-  if (i == 0) { i = imgSrcList.length }
-  i = i - 1;
-  slideImg.src = imgSrcList[i];
-}
+  function slidePrev() {
+    if (i == 0) { i = imgSrcList.length }
+    i = i - 1;
+    slideImg.src = imgSrcList[i];
+  }
 
-// next 버튼, prev 버튼 기능
-const prevBtn = document.getElementById("prevArrow");
-const nextBtn = document.getElementById("nextArrow");
-prevBtn.addEventListener("click", slidePrev);
-nextBtn.addEventListener("click", slideNext);
+  // next 버튼, prev 버튼 기능
+  const prevBtn = document.getElementById("prevArrow");
+  const nextBtn = document.getElementById("nextArrow");
+  prevBtn.addEventListener("click", slidePrev);
+  nextBtn.addEventListener("click", slideNext);
+
+
+};
+
+// 이미지 슬라이드를 생성해주는 객체 생성 코드. 현재는 맨 밑에줄에 있는 코드 하나만 실행되는데, 함수의 인자값으로 부모 div의 클래스를 받아서 그 클래스의 자식요소로 이미지슬라이드가 생성되도록 만들 예정.
+const imgSlide01 = new imgSlide("#000");
+const imgSlide02 = new imgSlide("#ddd");
+
 
 // 자동 슬라이드
 function autoSlide() {
